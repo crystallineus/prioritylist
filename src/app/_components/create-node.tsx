c606715fc72f1845cd9745330a6418477c23d7ca
Chien Shan (Christina) Peng <christinapeng5473@gmail.com>
2024-06-03 22:32:39 -0700

    Feature/create modal (#16)

--- src/app/_components/create-node.tsx
+++ src/app/_components/create-node.tsx
@@ -1,102 +1,132 @@
 "use client";
 
 import { useState } from "react";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input } from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 
-import { api } from "~/trpc/react";
+import { RouterOutputs, api } from "~/trpc/react";
 
 type CreateNodeProps = {
   parentId: string;
 }
+
+type Node = RouterOutputs['node']['listChildren'][number];
 
 export function CreateNode({ parentId }: CreateNodeProps) {
   const utils = api.useUtils();
   const [name, setName] = useState("");
   const [note, setNote] = useState("");
   const [page, setPage] = useState<"info" | "priority">("info");
-  const [idx, setIdx] = useState<number | "">();
-  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure({ onClose: () => { resetModal() } });
 
+  const listChildrenQuery = api.node.listChildren.useQuery({ parentId });
+  const nodes = listChildrenQuery.data ?? [];
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const resetModal = () => {
+    setPage("info");
+    setName("");
+    setNote("");
+    setCount(0);
+  }
   const createMutation = api.node.create.useMutation({
     async onSuccess() {
       await utils.node.listChildren.invalidate({ parentId });
       setName("");
       setNote("");
-      setIdx("");
     },
   });
 
-  const create = () => {
-    createMutation.mutate({ name, note, parentId, idx: idx === "" ? undefined : idx })
+  const create = (idx: number) => {
+    createMutation.mutate({ name, note, parentId, idx })
     onClose();
+  }
+
+  const changePage = () => {
+    if (page === "info") {
+      setLeft(0);
+      setRight(nodes.length);
+      setPage("priority");
+    } else {
+      setPage("info");
+    }
+  }
+
+  const mid = Math.floor((left + right) / 2);
+  const midNode = nodes[mid];
+
+  const handleLower = () => {
+    const nextLeft = mid + 1;
+    if (count === 3 || nextLeft === right) {
+      create(nextLeft);
+    } else {
+      setLeft(nextLeft);
+      setCount(count + 1);
+    }
+  }
+
+  const handleHigher = () => {
+    const nextRight = mid - 1;
+    if (count === 3 || nextRight < left) {
+      create(left);
+    } else {
+      setRight(nextRight);
+      setCount(count + 1);
+    }
   }
 
   return (
     <>
       <Button isIconOnly onPress={onOpen}><CreateIcon /></Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        {page === 'info' && (
-          <ModalContent>
-            {(onClose) => (
-              <>
-                <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                <ModalBody>
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="w-full rounded-full px-4 py-2 text-black"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Note"
-                    value={note}
-                    onChange={(e) => setNote(e.target.value)}
-                    className="w-full rounded-full px-4 py-2 text-black"
-                  />
-                  <input
-                    type="number"
-                    placeholder="Priority"
-                    value={idx}
-                    onChange={(e) => setIdx(parseInt(e.target.value))}
-                    className="w-full rounded-full px-4 py-2 text-black"
-                  />
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="danger" variant="light" onPress={onClose}>
-                    Close
-                  </Button>
-                  <Button color="primary" disabled={createMutation.isPending}
-                    onPress={create}>
-                    {createMutation.isPending ? "Creating..." : "Create Node"}
-                  </Button>
-                </ModalFooter>
-              </>
-            )}
-          </ModalContent>
-        )}
-        {page === 'priority' && (
-          <ModalContent>
-            {(onClose) => (
-              <>
-                <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                <ModalBody>
-                  TODO
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="danger" variant="light" onPress={onClose}>
-                    Close
-                  </Button>
-                  <Button color="primary" disabled={createMutation.isPending}
-                    onPress={create}>
-                    {createMutation.isPending ? "Creating..." : "Create Node"}
-                  </Button>
-                </ModalFooter>
-              </>
-            )}
-          </ModalContent>
-        )}
+        <ModalContent>
+          {page === 'info' && (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Create Item</ModalHeader>
+              <ModalBody>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full rounded-full px-4 py-2 text-black"
+                />
+                <input
+                  type="text"
+                  placeholder="Note"
+                  value={note}
+                  onChange={(e) => setNote(e.target.value)}
+                  className="w-full rounded-full px-4 py-2 text-black"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="default" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={changePage}>
+                  Next
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+          {page === 'priority' && (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Priority Item</ModalHeader>
+              <ModalBody>
+                {midNode?.name ?? "The node is going to be the first priority."}
+              </ModalBody>
+              <ModalFooter>
+                <Button onPress={handleLower}>
+                  Lower
+                </Button>
+                <Button onPress={handleHigher}>
+                  Higher
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
       </Modal>
     </>
   );
