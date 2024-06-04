9922b30e725cf5f9a20a9515e06dc24c226cf679
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-06-04 15:39:49 -0700

    Add missing migration (#28)

---
+++ drizzle/0010_gifted_bloodaxe.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "prioritylist-prototype_node" ADD COLUMN "url" text;--> statement-breakpoint
+ALTER TABLE "prioritylist-prototype_node" ADD COLUMN "urlPreviewImageUrl" text;--> statement-breakpoint
+ALTER TABLE "prioritylist-prototype_node" ADD COLUMN "urlPreviewDescription" text;
